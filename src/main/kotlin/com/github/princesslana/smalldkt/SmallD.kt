77f010ecd6d13e29088a2ca1dfe7a3a73ac5e2fd
Corey Shupe <coreyshupe101@gmail.com>
2018-12-14 07:48:37 -0500

    Moved to jdk-8 to allow for resource `use` Fixed AutoCloseable resource necessity by switching to jdk-8 which supports <T: AutoCloseable, R> T.use((T) -> R).

--- src/main/kotlin/com/github/princesslana/smalldkt/SmallD.kt
+++ src/main/kotlin/com/github/princesslana/smalldkt/SmallD.kt
@@ -1,23 +1,15 @@
 package com.github.princesslana.smalldkt
 
-import com.github.princesslana.smalld.SmallD
+import com.github.princesslana.smalld.SmallD as Bot
 
-private fun acceptBot(token: String, receiver: SmallD.() -> Unit): SmallD {
-    val smallD = SmallD.create(token)
-    receiver(smallD)
-    return smallD
+fun accept(token: String, receiver: Bot.() -> Unit, runner: Bot.() -> Unit) = Bot.create(token).use {
+    it.receiver()
+    it.runner()
+    it
 }
 
-fun startBot(token: String, receiver: SmallD.() -> Unit) = acceptBot(token, receiver).run()
+fun startBot(token: String, receiver: Bot.() -> Unit) = accept(token, receiver, Bot::run)
 
-fun startBotConnect(token: String, receiver: SmallD.() -> Unit): SmallD {
-    val smallD = acceptBot(token, receiver)
-    smallD.connect()
-    return smallD
-}
+fun startBotConnect(token: String, receiver: Bot.() -> Unit) = accept(token, receiver, Bot::connect)
 
-fun startBotAwait(token: String, receiver: SmallD.() -> Unit): SmallD {
-    val smallD = acceptBot(token, receiver)
-    smallD.await()
-    return smallD
-}
+fun startBotAwait(token: String, receiver: Bot.() -> Unit) = accept(token, receiver, Bot::await)
