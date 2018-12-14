68f2243dc4829f2f80cc493b8116f3233d488281
Corey Shupe <coreyshupe101@gmail.com>
2018-12-14 08:02:03 -0500

    Changed to overload func `startBot` as it's a better name.

--- src/main/kotlin/com/github/princesslana/smalldkt/SmallD.kt
+++ src/main/kotlin/com/github/princesslana/smalldkt/SmallD.kt
@@ -2,14 +2,16 @@ package com.github.princesslana.smalldkt
 
 import com.github.princesslana.smalld.SmallD as Bot
 
-fun accept(token: String, receiver: Bot.() -> Unit, runner: Bot.() -> Unit) = Bot.create(token).use {
+fun startBot(token: String, receiver: Bot.() -> Unit, runner: Bot.() -> Unit) = Bot.create(token).use {
     it.receiver()
     it.runner()
     it
 }
 
-fun startBot(token: String, receiver: Bot.() -> Unit) = accept(token, receiver, Bot::run)
+fun startBot(token: String, receiver: Bot.() -> Unit) = startBot(token, receiver, Bot::run)
 
-fun startBotConnect(token: String, receiver: Bot.() -> Unit) = accept(token, receiver, Bot::connect)
+fun startBotConnect(token: String, receiver: Bot.() -> Unit) =
+    startBot(token, receiver, Bot::connect)
 
-fun startBotAwait(token: String, receiver: Bot.() -> Unit) = accept(token, receiver, Bot::await)
+fun startBotAwait(token: String, receiver: Bot.() -> Unit) =
+    startBot(token, receiver, Bot::await)
