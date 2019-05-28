a9d7360ac431168dcd2dbf008fcd46623de92106
fixed <coreyshupe101@gmail.com>
2019-05-28 15:21:24 -0400

    Fixed to use proper JSON

--- src/main/kotlin/com/github/princesslana/smalldkt/Routes.kt
+++ src/main/kotlin/com/github/princesslana/smalldkt/Routes.kt
@@ -1,13 +1,9 @@
 package com.github.princesslana.smalldkt
 
 import com.github.princesslana.smalld.Attachment
-import com.github.princesslana.smalld.SmallD
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlinx.serialization.ImplicitReflectionSerializer
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.parse
 import kotlinx.serialization.stringify
 import kotlin.coroutines.resume
@@ -16,7 +12,7 @@ import kotlin.coroutines.suspendCoroutine
 @ImplicitReflectionSerializer
 internal suspend inline fun <reified V : Any> get(smallD: SmallDData, path: String): V =
     suspendCoroutine { continuation ->
-        continuation.resume(Json.parse(smallD.smallD.get(path)))
+        continuation.resume(JSON.parse(smallD.smallD.get(path)))
     }
 
 @ImplicitReflectionSerializer
@@ -26,7 +22,7 @@ internal suspend inline fun <reified K : Any, reified V : Any> post(
     path: String,
     vararg attachments: Attachment
 ): V = suspendCoroutine { continuation ->
-    continuation.resume(Json.parse(smallD.smallD.post(path, Json.stringify(payload), *attachments)))
+    continuation.resume(JSON.parse(smallD.smallD.post(path, JSON.stringify(payload), *attachments)))
 }
 
 @ImplicitReflectionSerializer
@@ -35,7 +31,7 @@ internal suspend inline fun <reified K : Any, reified V : Any> put(
     payload: K,
     path: String
 ): V = suspendCoroutine { continuation ->
-    continuation.resume(Json.parse(smallD.smallD.put(path, Json.stringify(payload))))
+    continuation.resume(JSON.parse(smallD.smallD.put(path, JSON.stringify(payload))))
 }
 
 @ImplicitReflectionSerializer
@@ -44,7 +40,7 @@ internal suspend inline fun <reified K : Any, reified V : Any> patch(
     payload: K,
     path: String
 ): V = suspendCoroutine { continuation ->
-    continuation.resume(Json.parse(smallD.smallD.patch(path, Json.stringify(payload))))
+    continuation.resume(JSON.parse(smallD.smallD.patch(path, JSON.stringify(payload))))
 }
 
 internal fun delete(smallD: SmallDData, path: String) {
