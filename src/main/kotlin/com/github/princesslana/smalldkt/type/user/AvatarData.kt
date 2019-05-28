bfb503b7c084e9567cb92e43e22babed5b869636
fixed <coreyshupe101@gmail.com>
2019-05-28 14:18:56 -0400

    Changed to suspension format for routes

--- src/main/kotlin/com/github/princesslana/smalldkt/type/user/AvatarData.kt
+++ src/main/kotlin/com/github/princesslana/smalldkt/type/user/AvatarData.kt
@@ -1,5 +1,8 @@
 package com.github.princesslana.smalldkt.type.user
 
 import com.github.princesslana.smalldkt.Wrapper
+import com.github.princesslana.smalldkt.WrapperSerializer
+import kotlinx.serialization.Serializable
 
+@Serializable(with = WrapperSerializer::class)
 class AvatarData(string: String) : Wrapper<String>(string)
