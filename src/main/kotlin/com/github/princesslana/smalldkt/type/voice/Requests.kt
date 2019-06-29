d07ba792ce370ccde8232c111db40a437b038072
fixed <coreyshupe101@gmail.com>
2019-06-29 02:37:56 -0400

    Finished voice route

--- src/main/kotlin/com/github/princesslana/smalldkt/type/voice/Requests.kt
+++ src/main/kotlin/com/github/princesslana/smalldkt/type/voice/Requests.kt
@@ -1,2 +1,8 @@
 package com.github.princesslana.smalldkt.type.voice
 
+import com.github.princesslana.smalldkt.SmallDData
+import com.github.princesslana.smalldkt.http.get
+import kotlinx.serialization.internal.ArrayListSerializer
+
+suspend fun SmallDData<*>.listVoiceRegions() =
+    smallD.get("/voice/regions", ArrayListSerializer(VoiceRegion.serializer()))
